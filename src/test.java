21eb8acda090bd1a8a6815b2829b7da9b26fc685
karthi1219 <karthikeyan2261998@gmail.com>
2021-04-28 15:08:36 -0700

    verified

---
+++ src/test.java
@@ -0,0 +1,22 @@
+
+public class test {
+	public static void main(String[] args) {
+		String s="madam";
+		String t="";
+		for(int i=s.length()-1;i>=0;i--) {
+		
+			t=t+s.charAt(i);
+		}
+		System.out.println(t);
+		
+		if(s==t)
+		{	
+		System.out.println("It is not a Palindrome");
+		}
+		else
+		{
+		System.out.println("It is  a Palindrome");
+		System.out.println("hii");
+		}
+		}
+}
